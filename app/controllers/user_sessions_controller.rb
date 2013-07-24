ac03b2d1cdc5eccfd51b309bd1c6a2617c1ef635
Eric Mooney <eric.c.mooney@gmail.com>
2013-07-24 14:31:49 -0700

    Add login via AJAX, working on rerendering nav

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -3,13 +3,18 @@ class UserSessionsController < ApplicationController
 
   def new
     @user = User.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.js
+    end
   end
 
   def create
     respond_to do |format|
       if @user = login(params[:email], params[:password])
         format.html { redirect_back_or_to(:users, :notice => 'Login successful.') }
-        format.xml { render :xml => @user, :status => :created, :location => @user }
+        format.js
       else
         format.html { flash.now[:alert] = "Login failed."; render :action => "new" }
         format.xml { render :xml => @user.errors, :status => :unprocessable_entity }
@@ -19,6 +24,9 @@ class UserSessionsController < ApplicationController
 
   def destroy
     logout
-    redirect_to(:users, :notice => 'Logged out!')
+    respond_to do |format|
+      format.html {redirect_to(:users, :notice => 'Logged out!')}
+      format.js
+    end
   end
 end
