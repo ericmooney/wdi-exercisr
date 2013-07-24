32a53054f7fbdc9fb55b760dfb7d15819b359244
Eric Mooney <eric.c.mooney@gmail.com>
2013-07-24 10:02:19 -0700

    Add exercise model and foundation gem

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'dotenv-rails'
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
+  gem 'zurb-foundation', '~> 2.2'
   gem 'sass-rails',   '~> 3.2.3'
   # gem 'coffee-rails', '~> 3.2.1'
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+  has_many :exercises
   attr_accessible :email, :password, :password_confirmation
 
   validates_length_of :password, :minimum => 3, :message => "password must be at least 3 characters long", :if => :password
