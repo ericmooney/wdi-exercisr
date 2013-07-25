7c508836b319b0c981489df058f5a7c661434669
Eric Mooney <eric.c.mooney@gmail.com>
2013-07-24 17:57:02 -0700

    Add graph to show user's exercises by day by activity

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,45 @@
 //= require_tree .
 
 $(document).foundation();
+
+$(document).ready(function(){
+
+  // The `ajax` block below should get called when you make the request for generating the chart.
+  var chart_display = $('#chart_display');
+
+  var generate_chart = function(){
+    chart_display.children().remove();
+    $.ajax({
+        dataType: "json",
+        type: "get",
+        url: "/exercises/chart/" + $('#activity_select').val()
+  }).done(graph_activity);
+  };
+
+  // "/exercises/chart/some_activity" routes to the exercises#chart controller action. It returns JSON.
+
+  // When the AJAX request is done, you will have that JSON response, which is passed to the 'done' callback function:
+
+  var graph_activity = function(exercise_data){
+    console.log(exercise_data);
+    chart_display.append('<div id="exercise_charted" class="graph"></div>');
+
+    Morris.Line({
+      element: 'exercise_charted',
+      data: exercise_data,
+      xkey: 'completed',
+      ykeys: ['value'],
+      labels: ['value'],
+      parseTime: false,
+      lineColors: ['red'],
+      hideHover: true
+    });
+  };
+
+
+
+  $('#chart_button').on("click", generate_chart);
+
+
+
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 WdiExercisr::Application.routes.draw do
   match 'login' => 'user_sessions#new', :as => :login
   match 'logout' => 'user_sessions#destroy', :as => :logout
+  get 'exercises/chart/:activity' => 'exercises#chart', :as => "chart"
+
   root :to => 'users#home'
 
   resources :user_sessions
